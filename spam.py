56e459d9d9fb81d2b6368137693741600203e7b9
snowy <thethinkingofficial@gmail.com>
2018-11-22 19:06:22 +0100

    Initial commit

---
+++ spam.py
@@ -0,0 +1,32 @@
+#! python2
+
+import requests
+import os
+import random
+import string
+import json
+
+chars = string.ascii_letters + string.digits + '!@#$%^&*()'
+random.seed = (os.urandom(1024))
+
+url = 'http://cheaters.batcave.net/?contact=new'
+
+names = json.loads(open('names.json').read())
+messages = json.loads(open('messages.json').read())
+
+for name in names:
+    name_extra = ''.join(random.choice(string.digits))
+
+    steamid = '76561198179913894'
+    email = name.lower() + name_extra + '@gmail.com'
+    message = random.choice(messages)
+    reason = 'report'
+
+    requests.post(url, allow_redirects=False, data ={
+        'first': steamid,
+        'mail': email,
+        'message_text': message,
+        'reason': reason
+    })
+
+    print 'Sending email [%s] with message [%s]' % (email, message)
